d6b98877d4562e16ae522c91002868b577823b7d
Bartek <bartek.planiak@gmail.com>
2021-01-16 00:20:33 +0100

    Add generate number functionality in main activity

--- app/src/main/java/com/example/randomnumber/MainActivity.kt
+++ app/src/main/java/com/example/randomnumber/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.randomnumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Generate Number
+        btnGenerateNumber.setOnClickListener{
+            val minNumber = minNumberToGenerate.text.toString().toInt()
+            val maxNumber = maxNumberToGenerate.text.toString().toInt()
+
+            val random = Random.nextInt(minNumber, maxNumber)
+            generatedNumber.text = random.toString()
+        }
+
     }
 }
