e011a447ed8b34c4fc0a3e159d1cc6ba2ae9fb1b
Bartek <bartek.planiak@gmail.com>
2021-01-18 00:06:24 +0100

    Create new randomNumber activity & move the code from Main activity there

---
+++ app/src/main/java/com/example/randomStuffGenerator/RandomNumberActivity.kt
@@ -0,0 +1,60 @@
+package com.example.randomStuffGenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_random_number.*
+import kotlin.random.Random
+
+class RandomNumberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random_number)
+
+        // Generate Number
+        btnGenerateNumber.setOnClickListener{
+            error.text = ""
+
+            val minNumber = when {
+                minNumberToGenerate.text.toString() == "" -> {
+                    Int.MIN_VALUE
+                }
+                minNumberToGenerate.text.toString().toIntOrNull() == null -> {
+                    error.text = getString(R.string.errorNumberIsTooSmallOrLarge)
+                    return@setOnClickListener
+                }
+                else -> {
+                    minNumberToGenerate.text.toString().toInt() }
+            }
+
+            val maxNumber = when {
+                maxNumberToGenerate.text.toString() == "" -> {
+                    Int.MAX_VALUE
+                }
+                maxNumberToGenerate.text.toString().toIntOrNull() == null -> {
+                    error.text = getString(R.string.errorNumberIsTooSmallOrLarge)
+                    return@setOnClickListener
+                }
+                else -> {
+                    maxNumberToGenerate.text.toString().toInt() }
+            }
+
+
+            // Error checker
+            when {
+                minNumber > maxNumber -> {
+                    error.text = getString(R.string.errorMinNumberGreaterThanMaxNumber)
+                    return@setOnClickListener
+                }
+                minNumber == maxNumber -> {
+                    error.text = getString(R.string.errorMinNumberEqualToMaxNumber)
+                    return@setOnClickListener
+                }
+                else -> {
+                    val random = Random.nextInt(minNumber, maxNumber)
+                    generatedNumber.text = random.toString()
+                }
+            }
+        }
+
+    }
+}
