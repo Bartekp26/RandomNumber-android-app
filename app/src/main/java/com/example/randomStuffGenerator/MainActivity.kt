a77161a0a65206a28488fdd5dbb47399ce613ddc
Bartek <bartek.planiak@gmail.com>
2021-01-20 00:39:01 +0100

    Add menu to action bar

--- app/src/main/java/com/example/randomStuffGenerator/MainActivity.kt
+++ app/src/main/java/com/example/randomStuffGenerator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.randomStuffGenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.menuAbout -> {
+                // TODO: add functionality to about item in menu
+            }
+            R.id.menuExit -> {
+                // TODO: add functionality to exit item in menu
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
