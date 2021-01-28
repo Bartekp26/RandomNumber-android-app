12a243f161c4dae545e844fc31ab4fc88a0f9846
Bartek <bartek.planiak@gmail.com>
2021-01-28 20:57:14 +0100

    Add layout for random letter activity

---
+++ app/src/main/java/com/example/randomStuffGenerator/RandomLetterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.randomStuffGenerator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RandomLetterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random_letter)
+    }
+}
